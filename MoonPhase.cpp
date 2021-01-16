644d5758aa11602423b70e385eee00c1196ab3a7
ahitech@gmail.com <ahitech@gmail.com>
2021-01-16 03:18:28 +0200

    Описание алгоритма.

--- MoonPhase.cpp
+++ MoonPhase.cpp
@@ -87,7 +87,14 @@ void MoonPhaseDisplay::GetPreferredSize (float *width, float *height)
  *	\brief		This function covers part of the Moon picture if requested.
  *	\param[in]	day	Current moon day (day in the current moon month)
  *	\details	Details of the algorithm:
- *				Moon month can be either 28, 29 or 30 days. 
+ *				Moon month can be either 29 or 30 days. I assume that it's
+ *				always 29 days. I start from the full circle that covers the
+ *				whole Moon's picture, then every day (starting with 1st)
+ *				until 14th I substract a part that goes from pole to pole and
+ * 				amounts to 1/14 of Moon's picture width at the widest point,
+ *				namely, the equator. The 15th day is always full moon (no
+ *				black part at all). Then I start to add black parts of the same
+ *				width on the right, and at 30th day it is full black again.
  */
 void MoonPhaseDisplay::DrawCover(unsigned int moonDay)
 {
